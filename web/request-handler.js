368a09c5ab261d81a7ceeb602d7448400fdae6d8
willchen90 <willchen90@gmail.com>
2014-10-01 16:33:10 -0700

    pseudo code router

--- web/request-handler.js
+++ web/request-handler.js
@@ -3,6 +3,31 @@ var archive = require('../helpers/archive-helpers');
 // require more modules/folders here!
 
 exports.handleRequest = function (req, res) {
+  // ROUTER
+  //  POST request - with URL in the body
+  //  GET request - static files
+  //
+  //  ROUTER
+  //  /
+  //  /archive/
+  var routes = {
+    '/': {
+      'GET': function(request, response){
+        // Serve the index.html
+      }
+    },
+    '/submit': {
+      'POST': function(request, response){
+        // Collect the data
+        // Process it
+        // If... submitted URL exists in archive
+          // Serve the file
+        // Else..
+          // Add to queue 'sites.txt'
+          // Redirect them to loading.html
+      }
+    }
+  };
 
   res.end('hello world');
   // res.end(archive.paths.list);
