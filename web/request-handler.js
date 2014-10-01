a3cede8e78e6bf0b41d62384e1d873b2e85c1a84
willchen90 <willchen90@gmail.com>
2014-10-01 16:41:39 -0700

    logic for path method

--- web/request-handler.js
+++ web/request-handler.js
@@ -1,40 +1,38 @@
+var url = require('url');
 var path = require('path');
 var archive = require('../helpers/archive-helpers');
 // require more modules/folders here!
 
-exports.handleRequest = function (req, res) {
-  // ROUTER
-  //  POST request - with URL in the body
-  //  GET request - static files
-  //
-  //  ROUTER
-  //  /
-  //  /archive/
-  var routes = {
-    '/': {
-      'GET': function(request, response){
-        // Serve the index.html
-      }
-    },
-    '/submit': {
-      'POST': function(request, response){
-        // Collect the data
-        // Process it
-        // If... submitted URL exists in archive
-          // Serve the file
-        // Else..
-          // Add to queue 'sites.txt'
-          // Redirect them to loading.html
-      }
+var routes = {
+  '/': {
+    'GET': function(request, response){
+      response.end('GET received');
+      // Serve the index.html
     }
-  };
+  },
+  '/submit': {
+    'POST': function(request, response){
+      response.end('POST received');
+      // Collect the data
+      // Process it
+      // If... submitted URL exists in archive
+        // Serve the file
+      // Else..
+        // Add to queue 'sites.txt'
+        // Redirect them to loading.html
+    }
+  }
+};
 
-  res.end('hello world');
-  // res.end(archive.paths.list);
-  //
+exports.handleRequest = function (request, response) {
+  var requestPath = url.parse(request.url).path;
+  var method = request.method;
 
-  //
-  //
-  //
-  //
+  if ( routes[requestPath] && routes[requestPath][method] ) {
+    routes[requestPath][method](request, response);
+  } else {
+    response.writeHead(404);
+    response.end('404 - Invalid Path');
+  }
+
 };
